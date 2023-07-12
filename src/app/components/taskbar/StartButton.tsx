c932df5c60843a90b461e4eed08cbdeb685a2764
sohailsayed <s7sayed@uwaterloo.ca>
2023-07-12 17:37:45 -0400

    get basic alignment with start and taskbar line with flexbox

--- src/app/components/taskbar/StartButton.tsx
+++ src/app/components/taskbar/StartButton.tsx
@@ -2,21 +2,7 @@ import Image from "next/image";
 import styles from "./taskbar.module.css";
 
 const StartButton = () => {
-  return (
-    <div
-      style={{
-        width: "100px",
-        height: "5vh",
-        position: "relative",
-      }}
-    >
-      <Image
-        src="/startButtonBackground.svg"
-        alt="Start button"
-        layout="fill"
-      />
-    </div>
-  );
+  return <img src="/startButtonBackground.svg" />;
 };
 
 export default StartButton;
