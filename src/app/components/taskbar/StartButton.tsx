2a8a047b48eb45a0b3eb3da96e0df0724f9d9620
sohailsayed <s7sayed@uwaterloo.ca>
2023-07-13 03:23:59 -0400

    make start button resizable horizontally

--- src/app/components/taskbar/StartButton.tsx
+++ src/app/components/taskbar/StartButton.tsx
@@ -1,8 +1,32 @@
-import Image from "next/image";
+// style module not working here for some reason, need to investigate, do inline styling for now
 import styles from "./taskbar.module.css";
 
 const StartButton = () => {
-  return <img src="/startButtonBackground.svg" />;
+  return (
+    <>
+      <section
+        style={{
+          gridArea: "startBack",
+          background: "url(/startBack.svg)",
+          backgroundSize: "cover",
+        }}
+      />
+      <section
+        style={{
+          gridArea: "startMiddle",
+          background: "url(/startMiddle.svg)",
+          backgroundSize: "auto 100%",
+        }}
+      />
+      <section
+        style={{
+          gridArea: "startEnd",
+          background: "url(/startEnd.svg)",
+          backgroundSize: "cover",
+        }}
+      />
+    </>
+  );
 };
 
 export default StartButton;
