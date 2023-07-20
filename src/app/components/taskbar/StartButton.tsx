0e87c64034d55b1bfd8163b935d63a2fc6cdb729
sohailsayed <s7sayed@uwaterloo.ca>
2023-07-19 23:16:24 -0400

    add styling for open start button

--- src/app/components/taskbar/StartButton.tsx
+++ src/app/components/taskbar/StartButton.tsx
@@ -12,19 +12,33 @@ const tahomaBold = localFont({ src: "../../fonts/tahoma/tahomabd.ttf" });
 
 const StartButton = () => {
   const [isHover, setIsHover] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  // Change boolean back and forth for now, temporary to test visual
+  const handleClick = () => {
+    setOpen(!open);
+    console.log(open);
+  };
 
   const handleHover = (hover: boolean) => {
     setIsHover(hover);
   };
 
   const StartMiddleSection = ({ startLabel }: Props) => {
+    // This part is overly verbose, but it works
+    const middleHoverCheck = isHover
+      ? styles.startMiddleSectionHover
+      : styles.startMiddleSection;
+    const middleOpenHoverCheck = isHover
+      ? styles.startMiddleSectionHoverOpen
+      : styles.startMiddleSectionOpen;
+
     return (
       <section
-        className={
-          isHover ? styles.startMiddleSectionHover : styles.startMiddleSection
-        }
+        className={open ? middleOpenHoverCheck : middleHoverCheck}
         onMouseEnter={() => handleHover(true)}
         onMouseLeave={() => handleHover(false)}
+        onClick={handleClick}
       >
         <img
           src="/icons/windowsXPIcon.png"
@@ -51,20 +65,31 @@ const StartButton = () => {
     );
   };
 
+  // This part is overly verbose, but it works
+  const backHoverCheck = isHover
+    ? styles.startBackSectionHover
+    : styles.startBackSection;
+  const backOpenHoverCheck = isHover
+    ? styles.startBackSectionHoverOpen
+    : styles.startBackSectionOpen;
+
+  const endHoverCheck = isHover
+    ? styles.startEndSectionHover
+    : styles.startEndSection;
+  const endOpenHoverCheck = isHover
+    ? styles.startEndSectionHoverOpen
+    : styles.startEndSectionOpen;
+
   return (
     <>
       <section
-        className={
-          isHover ? styles.startBackSectionHover : styles.startBackSection
-        }
+        className={open ? backOpenHoverCheck : backHoverCheck}
         onMouseEnter={() => handleHover(true)}
         onMouseLeave={() => handleHover(false)}
       />
       <StartMiddleSection startLabel="About Me" />
       <section
-        className={
-          isHover ? styles.startEndSectionHover : styles.startEndSection
-        }
+        className={open ? endOpenHoverCheck : endHoverCheck}
         onMouseEnter={() => handleHover(true)}
         onMouseLeave={() => handleHover(false)}
       />
