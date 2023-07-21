ff12d6ed3c2f2d1bcd42da4a447c6773274b88f7
sohailsayed <s7sayed@uwaterloo.ca>
2023-07-21 16:50:46 -0400

    create window border

--- src/app/components/window/TitleBar.tsx
+++ src/app/components/window/TitleBar.tsx
@@ -1,7 +1,20 @@
 import styles from "./window.module.css";
 
 const TitleBar = () => {
-  return <section className={styles.titleBar}></section>;
+  const TitleBarTop = () => {
+    return (
+      <section className={styles.titleBar}>
+        <p>Filler Text</p>
+      </section>
+    );
+  };
+
+  return (
+    <section className={styles.titleBarCombined}>
+      <TitleBarTop />
+      <section className={styles.titleBarBottom} />
+    </section>
+  );
 };
 
 export default TitleBar;
