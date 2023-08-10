1d030865f252bef91487e95ff7a035e7b6dc0904
sohailsayed <s7sayed@uwaterloo.ca>
2023-08-10 03:22:14 -0400

    add base right and left panels to main panel

--- src/app/components/start/MainPanel.tsx
+++ src/app/components/start/MainPanel.tsx
@@ -1,7 +1,12 @@
 import styles from "./start.module.css";
 
 const MainPanel = () => {
-  return <section className={styles.mainPanel}></section>;
+  return (
+    <section className={styles.mainPanel}>
+      <div className={styles.mainPanelLeft} />
+      <div className={styles.mainPanelRight} />
+    </section>
+  );
 };
 
 export default MainPanel;
